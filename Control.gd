efebf36f0e8bb89d2ef1ffb14b3e37c14bbf40f7
Samuel Williams <samuelhwilliams@gmail.com>
2020-05-10 12:01:20 +0100

    Minimal reproduction for HTML5 export low processor usage mode

---
+++ Control.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Label.text += str(OS.low_processor_usage_mode)
